c38a64f97905db5de265c07d3219cdf31dd25c1e
David Western <western@mines.edu>
2020-11-15 15:11:12 -0700

    Done with Vertical Runs
    Completed vertical_runs.py. This function performs vertical runs on a binary image and returns histograms for black and white runs.

---
+++ findStaves.py
@@ -0,0 +1,23 @@
+def findStaves():
+    # This program takes a sheet of music and finds the locations of the stafflines, their thickness, spacing, and ...
+
+    print("Looking for staves...")
+
+    # perform vertical run to find staffline thickness and spacing b/w the lines
+
+    # find the location of staves and the spacing between staves
+    # -- perform morphological ops
+    # -- then perform vertical runs
+
+
+def find_line_thickness(histogram):
+    # This function is to be used within vertical_runs.py
+
+
+def find_line_spacing():
+
+
+def find_staff_gaps():
+
+
+def find_staff_start_end():
--- vertical_runs.py
+++ vertical_runs.py
@@ -1,10 +1,15 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 
 def vertical_runs(img):
     # This function performs vertical runs on the entire piece of sheet music (or whatever image is passed to it)
-    # This function finds useful properties like staff spacing, staffline thickness, staffline spacing, and
-
+    # This function helps find useful properties like staffline thickness, staffline spacing, spacing between staves,
+    # and the height of a staff.
+    # OUTPUTS:
+    #   histograms of the lengths of black and white runs (uninterrupted lines of a single color)
+    # INPUTS:
+    #   a binary image of the entire piece of music or just a sliver
 
     # Image input must be a binary image!!!
     isBinaryImage = checkIfImageIsBinary(img)
@@ -14,7 +19,63 @@ def vertical_runs(img):
     width = img.shape[1]
     height = img.shape[0]
 
-    
+    white_data = []
+    black_data = []
+    pixel_count = 0
+    for col in range(0, width):
+        # set up some stuff here for vertical run
+        black_run = 0
+        white_run = 0
+
+        for row in range(0, height):
+            curr_pixel = img[row, col]
+            if curr_pixel == 0:     # check if current pixel is black
+                #print("Found black pixel")
+                # increase black run. stop and store white run
+                black_run += 1  # increase length of black pixel run
+                if white_run != 0:                  # reset white run when black pixel if it hasn't been yet
+                    white_data.append(white_run)    # store the length of the white pixel run
+                    white_run = 0                   # reset length of white run
+            elif curr_pixel == 255:
+                #print("Found White Pixel")
+                # increase white run. stop and store black run
+                white_run += 1
+                if black_run != 0:
+                    black_data.append(black_run)
+                    black_run = 0
+            else:
+                print("Whoops! Something went wrong. Pixel is not 0 or 255")
+
+        # terminate any current runs that aren't zero
+        if white_run > 0:
+            white_data.append(white_run)
+            pixel_count += 1
+        if black_run > 0:
+            black_data.append(black_run)
+
+    # create histogram for black and white runs.
+    hist_black = createHistogram(black_data, height)
+    hist_white = createHistogram(white_data, height)
+    return hist_black, hist_white
+
+
+def createHistogram(data, max_val):
+    histogram = np.zeros((max_val+1, 1))
+    pixel_count = 0
+    for value in data:
+        if value <= max_val:
+            if value !=0:
+                #print("Value is: ", value)
+                histogram[value] += 1
+                pixel_count += value
+                #print(histogram.T)
+        else:
+            print("Error! Run length is longer than the height of the image")
+            return None
+
+    #print("Histogram product sum is: ", pixel_count)
+    #print("")
+    return histogram
 
 
 def checkIfImageIsBinary(img, valueforWhite=255):
@@ -41,12 +102,36 @@ def checkIfImageIsBinary(img, valueforWhite=255):
 
 def main():
     print("Running test...")
-    test_img = cv.imread("test_staff_img_1.png", cv.IMREAD_GRAYSCALE)
-    _, test_img = cv.threshold(test_img, 150, 255, cv.THRESH_BINARY)
-    vertical_runs(test_img)
+    test_img = cv.imread("example_music_3.jpg", cv.IMREAD_GRAYSCALE)
+    _, test_img = cv.threshold(test_img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
 
+    # ---------  Use this to create custom image -------------
+    # shape_test_image = (256, 256)
+    # test_img = np.ones(shape_test_image)*255
+    # test_img[10:100, :] = np.zeros((1, 256))
+    # test_img[15, :] = np.zeros((1, 256))
+    # test_img[20, :] = np.zeros((1, 256))
+    # test_img[25, :] = np.zeros((1, 256))
+    # test_img[30, :] = np.zeros((1, 256))
+    # cv.imshow("Test image", test_img)
+    # cv.waitKey(0)
+
+    bh, wh = vertical_runs(test_img)
+
+    cv.imshow("Test Image", test_img)
+    cv.waitKey(0)
+    plt.plot(wh)
+    plt.show()
+
+    # check if histogram got all the pixels
+    combined_histogram = np.add(bh, wh)
+    width = test_img.shape[1]
+    height = test_img.shape[0]
+    print("Should see ", width*height, " pixels...")
+    print(wh)
 
     print("Test completed!")
+
 
 if __name__ == "__main__":
     main()
