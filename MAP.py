56000d1a7ef81353c7fd0aea03d444d78e4c51bd
David Western <western@mines.edu>
2020-11-30 20:12:20 -0700

    Removed Some outputs for MAP
    MAP was outputting some debugging info and it isn't needed

--- MAP.py
+++ MAP.py
@@ -12,11 +12,11 @@ def main():
     _, test_img = cv.threshold(test_img, 180, 255, cv.THRESH_BINARY)
 
     # create window to look at test image and click on pixels for verification purposes
-    picked_point = []
-    pick_window = "Test Image"
-    cv.namedWindow(pick_window, cv.WINDOW_NORMAL)
-    cv.setMouseCallback(pick_window, utilities_cv.get_xy, picked_point)
-    cv.imshow(pick_window, test_img)
+    # picked_point = []
+    # pick_window = "Test Image"
+    # cv.namedWindow(pick_window, cv.WINDOW_NORMAL)
+    # cv.setMouseCallback(pick_window, utilities_cv.get_xy, picked_point)
+    # cv.imshow(pick_window, test_img)
 
     # find staves
     staves = findStaves.findStaves(test_img)
@@ -27,7 +27,7 @@ def main():
     print("Removed staves")
 
     # look at image with no staff lines
-    cv.imshow("Staves Removes", image_no_staff)
+    # cv.imshow("Staves Removes", image_no_staff)
 
 
     cv.waitKey(0)
