19ca6806ffda672d1cddc0c00ce399678a04502e
David Western <western@mines.edu>
2020-11-30 20:43:26 -0700

    Removing unnecessary outputs
    removed unnecessary outputs from MAP

--- MAP.py
+++ MAP.py
@@ -15,29 +15,27 @@ def main():
     _, test_img = cv.threshold(test_img, 180, 255, cv.THRESH_BINARY)
 
     # create window to look at test image and click on pixels for verification purposes
-    picked_point = []
-    pick_window = "Test Image"
-    cv.namedWindow(pick_window, cv.WINDOW_NORMAL)
-    cv.setMouseCallback(pick_window, utilities_cv.get_xy, picked_point)
-    cv.imshow(pick_window, test_img)
+    # picked_point = []
+    # pick_window = "Test Image"
+    # cv.namedWindow(pick_window, cv.WINDOW_NORMAL)
+    # cv.setMouseCallback(pick_window, utilities_cv.get_xy, picked_point)
+    # cv.imshow(pick_window, test_img)
 
     # find staves
     staves = findStaves.findStaves(test_img)
-    print(" Found Staves")
+    print("Found Staves...")
 
     # remove staves
     image_no_staff = removeStaffLines.removeLines(test_img, staves)
     print("Removed staves")
 
     # look at image with no staff lines
-    cv.imshow("Staves Removes", image_no_staff)
-    #print(image_no_staff)
+    # cv.imshow("Staves Removes", image_no_staff)
+    # cv.waitKey(0)
 
-    cv.waitKey(0)
-
-    imagesdf = noteDetect.noteDetect()
-    cv.imshow(imagesdf)
-    cv.waitKey(0)
+    # imagesdf = noteDetect.noteDetect(staves, image_no_staff)
+    # cv.imshow(imagesdf)
+    # cv.waitKey(0)
     print("done")
 
 
