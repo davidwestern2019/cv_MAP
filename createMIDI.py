132dc116d6e4c30ca413d834542dce24b38243fb
David Western <western@mines.edu>
2020-12-05 14:47:27 -0700

    Write and Play MIDI
    Made a sample function to write midi music files. Created a module that can play music from a MIDI file.

--- createMIDI.py
+++ createMIDI.py
@@ -6,8 +6,8 @@ track    = 0
 channel  = 0
 time     = 0    # In beats
 duration = 1    # In beats
-tempo    = 60   # In BPM
-volume   = 100  # 0-127, as per the MIDI standard
+tempo    = 210   # In BPM
+volume   = 30  # 0-127, as per the MIDI standard
 
 MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
                       # automatically)
---
+++ playMIDI.py
@@ -0,0 +1,36 @@
+import pygame
+import io
+
+def play_music_file(music_file):
+    """
+    stream music with mixer.music module in blocking manner
+    this will stream the sound from disk while playing
+    """
+    pygame.mixer.music.load(music_file)
+    clock = pygame.time.Clock()
+    pygame.mixer.music.play()
+    # check if playback has finished
+    while pygame.mixer.music.get_busy():
+        clock.tick(30)
+
+
+def play_music(music_file):
+    # this function takes in a string MIDI filename and plays it.
+    # initialize pygame mixer
+    pygame.mixer.init()
+
+    # optional volume 0 to 1.0
+    pygame.mixer.music.set_volume(0.9)
+
+    # use the midi file you just saved
+    play_music_file(music_file)
+
+
+def main():
+    music_file = "major-scale.mid"
+
+
+
+
+if __name__ == '__main__':
+    main()
